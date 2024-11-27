e6e199d3ccba932a82310ee1f7f7f92d06e13b3f
akumi07 <akash9338144396@gmail.com>
2024-11-27 22:50:28 +0530

    fixed contact us issue

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,9 @@ import Update from "./components/Update";
 import UpdateUser from "./components/Update";
 import Pricing from "./components/Pricing";
 import Leadership from "./components/Leadership";
-import ContactUS from "./components/contactUS";
+import ContactUS from "./components/contactUS"
+
+
 
   function App() {
   return (
@@ -26,8 +28,8 @@ import ContactUS from "./components/contactUS";
         <Route path="/update-user"element={<UpdateUser/>}/>
         <Route path="/pricing" element={<Pricing/>}/>
         <Route path="/leadership" element={<Leadership/>}/>
-        <Route path="/contact" element={<ContactUS/>}/>
-      </Routes>
+        <Route path='/contact'element={<ContactUS/>}/>
+        </Routes>
     </Router>
     </LoginProvider>
   )
